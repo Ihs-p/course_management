869d4f6f8c3d3d03d15e3c18217050dba1be3ab1
Ihsan p <ihsanp3553@gmail.com>
2024-11-18 23:23:38 +0530

    Update settings.py

--- course_managment/course_managment/settings.py
+++ course_managment/course_managment/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'course_managment.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'course_management',
         'HOST':'localhost',
         'USER':'root',
